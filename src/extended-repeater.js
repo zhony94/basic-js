d0bd471e0d563c402e68799151f771c530fe878b
zhony94 <64091893+zhony94@users.noreply.github.com>
2022-04-28 11:16:33 +0500

    Update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let strRepTime = 1
+  let addRepTime = 1
+  let separatorSign = '+'
+  let addSeparatorSign = '|'
+  let additionStr = ''
+  let mainStr = String(str)
+  if(options.hasOwnProperty('repeatTimes')){
+    strRepTime = options.repeatTimes
+  }
+  if(options.hasOwnProperty('additionRepeatTimes')){
+    addRepTime = options.additionRepeatTimes
+  }
+  if(options.hasOwnProperty('separator')){
+    separatorSign = options.separator
+  }
+  if(options.hasOwnProperty('additionSeparator')){
+    addSeparatorSign = options.additionSeparator 
+  }
+  if(options.hasOwnProperty('addition')){
+    additionStr = String(options.addition)
+  }
+  
+  let additionText = []
+  for (i=0; i<addRepTime; i++){
+    additionText[i] = additionStr
+  }
+  const finalAddText = additionText.join(addSeparatorSign)
+  
+  let mainText = []
+  for (i=0; i<strRepTime; i++){
+    mainText[i] = mainStr + finalAddText
+  }
+  const finalMainText = mainText.join(separatorSign)
+  return finalMainText  
 }
 
 module.exports = {
