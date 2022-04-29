fd0bebe6180d2ab0ee76162f52ed595753e854d5
zhony94 <64091893+zhony94@users.noreply.github.com>
2022-04-29 15:29:05 +0500

    Update file-names.js

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  let original = []
+  let obj = {}
+  for (let j=0; j<names.length; j++){
+    original[j] = names[j]
+  }  
+  for (let i=0; i<names.length; i++){
+    let numbers = []
+    if(!obj.hasOwnProperty(names[i])){
+      
+      for(let m=0; m<names.length; m++){        
+        if(names[m] === names[i]){          
+          numbers.push(m)
+        }        
+      }
+      obj[names[i]] = numbers;
+      numbers = []
+    }    
+  }  
+  for (let key in obj){
+    for (let k=0; k<obj[key].length; k++){ 
+        if(k===0){
+          console.log(key)
+            console.log(key.slice(0,-3))
+              console.log(obj.hasOwnProperty(key.slice(0,-3)))
+          if(key.includes('(') && obj.hasOwnProperty(key.slice(0,-3)) && key.slice(0,-3).length>1){
+            original[obj[key][k]] = key + '(1)'
+          }else{
+            original[obj[key][k]] = key
+          }        
+        }else{
+        original[obj[key][k]] = key+'('+k+')'
+        }               
+      }
+    } 
+  return original
 }
 
 module.exports = {
