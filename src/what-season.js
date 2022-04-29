a9d9c2af3f22dd22159423b3c1e7a83ff7e9fe97
zhony94 <64091893+zhony94@users.noreply.github.com>
2022-04-29 18:01:12 +0500

    Update what-season.js

--- src/what-season.js
+++ src/what-season.js
@@ -11,18 +11,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(date) {    
-  let monthSelector = date.getMonth();
-if(monthSelector >= 0 && monthSelector <=1 || monthSelector === 11){
-  return 'winter'
-}else if (monthSelector > 1 && monthSelector <5){
-  return 'spring'
-}else if (monthSelector>4 && monthSelector<8){
-  return 'summer'
-}else if (monthSelector>7 && monthSelector<11){
-  return 'autumn'
-}
+function getSeason(date) {
+  if(typeof(date) !== 'undefined'){
+    if(isValidDate(date)){
+      let monthSelector = date.getMonth();
+  if (typeof(date)==='object' && date !== ''){
+    
+    if(monthSelector >= 0 && monthSelector <=1 || monthSelector === 11){
+      return 'winter'
+    }else if (monthSelector > 1 && monthSelector <5){
+      return 'spring'
+    }else if (monthSelector>4 && monthSelector<8){
+      return 'summer'
+    }else if (monthSelector>7 && monthSelector<11){
+      return 'autumn'
+    }
+  }
+    }else{
+      return "Invalid date!"
+    }
+    
+  }else {
+    return 'Unable to determine the time of year!'
+  }
  
+ 
+}
+function isValidDate(date) {
+  return date && Object.prototype.toString.call(date) === "[object Date]" && !isNaN(date);
 }
 
 module.exports = {
