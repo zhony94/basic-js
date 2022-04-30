65fd3a074b9d8d288337185821db88419dfd6e1c
zhony94 <64091893+zhony94@users.noreply.github.com>
2022-04-30 11:13:23 +0500

    Update vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(directMachine = true){
+    this.directMachine = directMachine
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(plainMsg, key ) {
+    if(plainMsg === undefined || key === undefined || typeof(plainMsg) !== "string" || typeof(key) !== "string") throw new Error('Incorrect arguments!')
+    let encrypted  = "";
+    let j=0;
+    for(let i = 0; i < plainMsg.length; i++){
+      let currentLetter = plainMsg[i];
+      let pi = currentLetter.toUpperCase().charCodeAt()
+      let ki = key[j%key.length].toUpperCase().charCodeAt()
+      if(pi >= 65 && pi <= 90){
+        let upperLetter = (((pi + ki) % 26) + 26) % 26;
+        encrypted  += String.fromCharCode(upperLetter + 65);
+        j++;
+      }else{
+         encrypted  += currentLetter;
+      }    
+    };
+
+    return (this.directMachine) ? encrypted : encrypted.split('').reverse().join('')
+  }
+  decrypt(plainMsg, key) {
+
+    if(plainMsg === undefined || key === undefined || typeof(plainMsg) !== "string" || typeof(key) !== "string") throw new Error('Incorrect arguments!')
+
+
+    let decrypted   = "";
+    let j=0;
+    for(let i = 0; i < plainMsg.length; i++){
+      let currentLetter = plainMsg[i];
+      let ci = currentLetter.toUpperCase().charCodeAt()
+      let ki = key[j%key.length].toUpperCase().charCodeAt()
+      if(ci >= 65 && ci <= 90){
+        let upperLetter = (((ci - ki) % 26) + 26) % 26;
+        decrypted   += String.fromCharCode(upperLetter + 65);
+        j++;
+      }else{
+        decrypted   += currentLetter;
+      }    
+    };
+
+    return (this.directMachine) ? decrypted : decrypted.split('').reverse().join('') 
   }
 }
 
